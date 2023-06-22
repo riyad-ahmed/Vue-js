9d0ffff5c073d081c9ddf12e4abedfd09211b248
riyadahmed2809@gmail.com <92542706+riyad-ahmed@users.noreply.github.com>
2023-06-23 00:18:22 +0600

    first commit

---
+++ public/vue.js
@@ -0,0 +1,11 @@
+// import { createApp } from 'vue'
+// import App from './App.vue'
+
+// createApp({
+//   setup() {
+//     return {
+//       count: ref(0)
+//     }
+//   }
+// })
+// createApp(App).mount('#app')
--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
-import { createApp } from 'vue'
 import App from './App.vue'
+import { createApp, ref } from 'vue'
+
+
+createApp({
+  setup() {
+    return {
+      count: ref(0)
+    }
+  }
+})
 
 createApp(App).mount('#app')
