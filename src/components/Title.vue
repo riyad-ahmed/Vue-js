15d57a9570bee10201f352821ed12473a65e7bb2
riyadahmed2809@gmail.com <92542706+riyad-ahmed@users.noreply.github.com>
2023-07-03 23:34:01 +0600

    add getdata

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -3,19 +3,28 @@
   <h1>Email : {{email}}</h1>
   <h1>{{Num==554}}</h1>
   <p>Addition {{59+20}}</p>
+  <h1>sumData : {{getdata()}}</h1>
+    <h1>sumData : {{getdata().email}}</h1>
 </div>
 </template>
 
 <script>
 export default {
-  name: 'Title',
-  data ()
-  {
-    return {
-      email: 'riyadahmed5764@gmail.com',
-      Num: '555'
-    }
-  }
+    name: 'Title',
+    data() {
+      return {
+        email: 'riyadahmed5764@gmail.com',
+        Num: '555'
+      }
+    },
+    methods: {
+      getdata() {
+        return {
+          name: 'Riyad Ahmed',
+          email: this.email,
+        }
+      }
+    }  
 }
 </script>
 
